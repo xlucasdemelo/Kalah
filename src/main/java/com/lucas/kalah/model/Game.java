21a5f07c73bc95568d15a249ba25d4666f0f7f26
xlucasdemelo <lucas.meloaguiar@gmail.com>
2020-06-16 19:25:50 -0300

    Move seeds logic

--- src/main/java/com/lucas/kalah/model/Game.java
+++ src/main/java/com/lucas/kalah/model/Game.java
@@ -6,6 +6,7 @@ import com.lucas.kalah.util.KalahConstants;
 import lombok.Data;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Class that will change the state of the Board
@@ -24,7 +25,26 @@ public class Game {
         final int houseIndex = turn.getHouseIndex();
         final House movePit = (House) this.board.getPits().get(houseIndex);
 
-        for (int i = houseIndex + 1; i < movePit.getNumberOfSeeds() + 1; i++){
+        int lastPit = houseIndex + movePit.getNumberOfSeeds();
+
+        for (int i = houseIndex + 1; i < lastPit + 1; i++){
+            if ( i > 13 ){
+               i = 0;
+               lastPit = lastPit - 13;
+            }
+
+            int enemyEndZone =
+                    this.players.stream()
+                    .filter( player -> player.getName() != turn.getPlayer().getName() )
+                    .collect(Collectors.toList())
+                    .get(0)
+                    .getLastPitIndex();
+
+            if (i == enemyEndZone) {
+                lastPit ++;
+                continue;
+            };
+
             final Pit pit = this.board.getPits().get(i);
             pit.receiveSeeds(1);
         }
--- src/test/java/com/lucas/kalah/GameTest.java
+++ src/test/java/com/lucas/kalah/GameTest.java
@@ -50,4 +50,170 @@ public class GameTest {
         assertThat(board.getPits().get(4).getNumberOfSeeds(), is(5) );
     }
 
+    @Test
+    public void PlayerOneMovesFourthIndex_end_zone_must_have_one_value_and_enemy_must_have_one_more(){
+        Game game = new Game(this.players);
+
+        final Turn turn = new Turn(this.players.get(0), 3);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(3).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(4).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(5).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(6).getNumberOfSeeds(), is(1) );
+        assertThat(board.getPits().get(7).getNumberOfSeeds(), is(5) );
+    }
+
+    @Test
+    public void PlayerOneMoves8SeedsFromSixthIndex_must_jump_enemies_end_zone(){
+        Game game = new Game(this.players);
+
+        game.getBoard().getPits().get(5).setNumberOfSeeds(8);
+
+        final Turn turn = new Turn(this.players.get(0), 5);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(5).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(6).getNumberOfSeeds(), is(1) );
+        assertThat(board.getPits().get(7).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(8).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(9).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(10).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(11).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(12).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(13).getNumberOfSeeds(), is(0 ));
+        assertThat(board.getPits().get(0).getNumberOfSeeds(), is(5) );
+    }
+
+    @Test
+    public void PlayerOneMoves10SeedsFromSixthIndex_must_jump_enemies_end_zone(){
+        Game game = new Game(this.players);
+
+        game.getBoard().getPits().get(5).setNumberOfSeeds(10);
+
+        final Turn turn = new Turn(this.players.get(0), 5);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(5).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(6).getNumberOfSeeds(), is(1) );
+        assertThat(board.getPits().get(7).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(8).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(9).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(10).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(11).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(12).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(13).getNumberOfSeeds(), is(0 ));
+        assertThat(board.getPits().get(0).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(1).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(2).getNumberOfSeeds(), is(5) );
+    }
+
+    @Test
+    public void PlayerTwoMovesFirstIndex_4Next_pits_must_increase(){
+        Game game = new Game(this.players);
+
+        final Turn turn = new Turn(this.players.get(1), 7);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(7).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(8).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(9).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(10).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(11).getNumberOfSeeds(), is(5) );
+    }
+
+    @Test
+    public void PlayerTwoMovesThirdIndex_4Next_pits_must_increase_end_zone_must_have_one_value(){
+        Game game = new Game(this.players);
+
+        final Turn turn = new Turn(this.players.get(1), 9);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(9).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(10).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(11).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(12).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(13).getNumberOfSeeds(), is(1) );
+    }
+
+    @Test
+    public void PlayerTwoMovesFourthIndex_end_zone_must_have_one_value_and_enemy_must_have_one_more(){
+        Game game = new Game(this.players);
+
+        final Turn turn = new Turn(this.players.get(1), 10);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(10).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(11).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(12).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(13).getNumberOfSeeds(), is(1) );
+        assertThat(board.getPits().get(1).getNumberOfSeeds(), is(5) );
+    }
+
+    @Test
+    public void PlayerTwoMovesFifthIndex_end_zone_must_have_one_value_and_enemy_must_have_two_more(){
+        Game game = new Game(this.players);
+
+        final Turn turn = new Turn(this.players.get(1), 11);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(11).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(12).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(13).getNumberOfSeeds(), is(1) );
+        assertThat(board.getPits().get(0).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(1).getNumberOfSeeds(), is(5) );
+    }
+
+    @Test
+    public void PlayerTwoMoves8SeedsFromTwelveIndex_must_jump_enemies_end_zone(){
+        Game game = new Game(this.players);
+
+        game.getBoard().getPits().get(12).setNumberOfSeeds(8);
+
+        final Turn turn = new Turn(this.players.get(1), 12);
+
+        game.moveSeeds(turn);
+
+        Board board = game.getBoard();
+
+        assertThat(board, notNullValue());
+        assertThat(board.getPits().get(12).getNumberOfSeeds(), is(0) );
+        assertThat(board.getPits().get(13).getNumberOfSeeds(), is(1) );
+        assertThat(board.getPits().get(0).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(1).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(2).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(3).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(4).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(5).getNumberOfSeeds(), is(5) );
+        assertThat(board.getPits().get(6).getNumberOfSeeds(), is(0 ));
+        assertThat(board.getPits().get(7).getNumberOfSeeds(), is(5) );
+    }
 }
